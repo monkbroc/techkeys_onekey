b81e53820dce45f7633a5791d903dd8f388c2f97
Julien Vanier <jvanier@gmail.com>
2014-04-26 13:19:05 -0400

    First working version

---
+++ Makefile
@@ -0,0 +1,59 @@
+SHELL=/bin/bash
+.DEFAULT_GOAL := all
+# Clear suffixes to disable implicit rules
+.SUFFIXES:
+include Makefile.config
+
+ifeq ($(OS), Windows_NT)
+	RM = rmdir /s /q
+	MKDIR = mkdir
+	fix_path = $(subst /,\,$1)
+else
+	RM = rm -fr
+	MKDIR = mkdir -p
+	fix_path = $1
+endif
+
+obj/techkeys:
+	$(MKDIR) $(call fix_path, obj/techkeys)
+obj/techkeys/%.d: %.c | obj/techkeys
+	@$(CC) $(CFLAGS) $(OPTS_techkeys) -DPLATFORM_techkeys -MM -MT "$(@:.d=.o) $@" $< > $@
+obj/techkeys/%.o: %.c | obj/techkeys
+	$(CC) $(CFLAGS) $(OPTS_techkeys) -DPLATFORM_techkeys -c -o $@ $<
+
+#define INCLUDE_TEMPLATE=
+#-include $$(SOURCES_$1:%.c=obj/$1/%.d) $$(BASE_SOURCES:%.c=obj/$1/%.d)
+#endef
+#$(foreach target,$(TARGETS),$(eval $(call INCLUDE_TEMPLATE,$(target))))
+
+bin/$(RESULT_techkeys).elf: $(SOURCES_techkeys:%.c=obj/techkeys/%.o) $(BASE_SOURCES:%.c=obj/techkeys/%.o) | bin/
+	$(CC) $(CFLAGS) $(LDFLAGS) $(OPTS_techkeys) -o $@ $^
+
+bin/:
+	$(MKDIR) bin
+
+bin/%.hex: bin/%.elf
+	avr-objcopy -O ihex $^ $@
+
+.PHONY: techkeys
+techkeys: bin/$(RESULT_techkeys).hex
+
+.PHONY: flash
+# Must have DFU programmer installed
+# http://dfu-programmer.sourceforge.net/
+# With the techkey plugged in the computer's USB, short circuit the
+# flash pin (next to the USB connector) for 1 second
+# Check that the device appears in lsusb as ID 03eb:2fef Atmel Corp
+# Then make flash to program
+flash:
+	sudo dfu-programmer $(MICROCONTROLLER) erase
+	sudo dfu-programmer $(MICROCONTROLLER) flash bin/$(RESULT_techkeys).hex
+	sudo dfu-programmer $(MICROCONTROLLER) start
+
+
+
+.PHONY: clean all
+all: $(TARGETS)
+
+clean:
+	$(RM) bin obj
---
+++ main_techkeys.c
@@ -0,0 +1,146 @@
+#include "platforms.h"
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "main.h"
+#include "usb_keyboard.h"
+#include "io.h"
+#include "hid.h"
+#include "timer.h"
+#include "auxiliary.h"
+
+#define NUM_KEYS 3
+
+#define BLUE_LED 0
+#define KEY1_LED 3
+#define KEY2_LED 2
+#define KEY3_LED 1
+#define KEY1_SWITCH 6
+#define KEY2_SWITCH 5
+#define KEY3_SWITCH 4
+
+#define DEBOUNCE_TIMER 10
+
+#define LED_ON false
+#define LED_OFF true
+
+const char allLeds[] = { BLUE_LED, KEY1_LED, KEY2_LED, KEY3_LED };
+
+const struct keysConfig_t {
+	char switchPin;
+	char ledPin;
+	int keycode;
+} keysConfig[NUM_KEYS] = {
+	{ KEY1_SWITCH, KEY1_LED, KCTRL},
+	{ KEY2_SWITCH, KEY2_LED, KALT},
+	{ KEY3_SWITCH, KEY3_LED, KDELETE},
+};
+
+struct keys_t {
+	bool state;
+	bool fallingEdge;
+	bool risingEdge;
+	char debounceTime;
+} keys[NUM_KEYS];
+
+void initLeds() {
+	for(unsigned char i = 0; i < sizeof(allLeds)/sizeof(allLeds[0]); i++) {
+		IO_set(i, LED_OFF);
+		IO_config(i, OUTPUT);
+	}
+}
+
+void initKeys() {
+	memset(&keys, sizeof(keys), 0);
+	for(unsigned char i = 0; i < NUM_KEYS; i++) {
+		IO_config(keysConfig[i].switchPin, INPUT);
+		IO_set(keysConfig[i].switchPin, true);
+	}
+}
+
+void debounce() {
+	for(unsigned char i = 0; i < NUM_KEYS; i++) {
+		char newState;
+		keys[i].fallingEdge = false;
+		keys[i].risingEdge = false;
+
+		newState = IO_get(keysConfig[i].switchPin);
+		if(keys[i].debounceTime > 0) {
+			keys[i].debounceTime--;
+		} else if(!keys[i].state && newState) {
+			keys[i].risingEdge = true;
+			keys[i].state = true;
+			keys[i].debounceTime = DEBOUNCE_TIMER;
+		} else if(keys[i].state && !newState) {
+			keys[i].fallingEdge = true;
+			keys[i].state = false;
+			keys[i].debounceTime = DEBOUNCE_TIMER;
+		}
+	}
+}
+
+void sendKey(int code, bool state) {
+	HID_set_scancode_state(code, state);
+	HID_commit_state();
+	_delay_ms(3);
+}
+
+void setLed(int pin, bool state) {
+	IO_set(pin, state);
+}
+
+void sendKeys() {
+	for(unsigned char i = 0; i < NUM_KEYS; i++) {
+		if(keys[i].fallingEdge) {
+			sendKey(keysConfig[i].keycode, true);
+			setLed(keysConfig[i].ledPin, LED_ON);
+		} else if(keys[i].risingEdge) {
+			sendKey(keysConfig[i].keycode, false);
+			setLed(keysConfig[i].ledPin, LED_OFF);
+		}
+	}
+}
+
+void setup() {
+	clock_prescale_set(clock_div_1);
+
+	USB_init();
+	HID_commit_state();
+
+	initLeds();
+	initKeys();
+
+	TIMER_init();
+}
+
+void loop() {
+	debounce();
+
+	sendKeys();
+	_delay_ms(1);
+}
+
+int main(void)
+{
+	setup();
+
+	for(;;) {
+		loop();
+	}
+
+}
+
+void MAIN_timer_handler()
+{
+	//for (int i = 0; i < 3; ++i)
+	//	IO_set(1+i, IO_get(4+i));
+}
+
+void MAIN_handle_sof()
+{
+}
